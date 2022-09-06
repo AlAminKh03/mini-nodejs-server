22a2bfec6b3b4de5903f73b87b0d579d2fcfd2e1
AlAminKh03 <alaminkhan6203@gmail.com>
2022-09-06 23:03:27 +0600

    final i guess

--- Routes/route.js
+++ Routes/route.js
@@ -1,12 +1,2 @@
-const express = require('express')
-const router = express.Router()
-const { randomUser, allUser, createUser, updateUser, bulkUpdate, deleteUser } = require('../Controllers/controllers')
 
-router.get('/user/random', randomUser)
-router.get('/user/all', allUser)
-router.post('/user/save', createUser)
-router.patch('/user/update', updateUser)
-router.patch('/user/bulk-upadate', bulkUpdate)
-router.delete('/user/delete', deleteUser)
 
-module.exports = router
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const cors = require('cors')
+import { readFileSync, writeFileSync } from 'fs';
+import express from "express"
+import path from 'path';
+import cors from 'cors';
 const port = process.env.port || 5000;
 const app = express();
-const bodyParser = require('body-parser');
-const useRouter = require("./Routes/route")
+import fs from 'fs'
+import bodyParser from 'body-parser'
+// const file = path.join(process.cwd(), 'users.json');
+
+
+
+
 
 
 
@@ -13,10 +20,219 @@ app.use(bodyParser.json())
 
 
 // 1.get a random user 
-app.use('/', useRouter)
 
 
+
+const randomUser = (req, res) => {
+    fs.readFile("users.json", (err, data) => {
+        if (err) {
+            res.send('data was not found')
+        } else {
+            const users = JSON.parse(data)
+            const randomUser = users[Math.floor(Math.random() * users.length) + 1]
+            res.send(JSON.stringify(randomUser))
+        }
+    })
+}
+
+
+// 2.getting all users 
+const allUser = (req, res) => {
+    const { limit } = req.query;
+
+    fs.readFile("users.json", (err, data) => {
+        if (err) {
+            res.send('data was not found')
+
+        } else {
+            const parsedData = JSON.parse(data)
+            const selectedUsers = parsedData.slice(0, limit ? limit : Infinity)
+            res.status(200).send(JSON.stringify(selectedUsers))
+        }
+    })
+}
+
+
+// 3. posting users 
+const createUser = (req, res) => {
+
+    const { id, name, gender, contact, address, photoUrl } = req.body;
+    console.log(id)
+
+    fs.readFile("users.json", (err, data) => {
+
+        if (err) {
+            res.send('data was not found')
+        }
+
+        else {
+            const allUser = JSON.parse(data);
+            if (id && name && gender && contact && address && photoUrl) {
+                const existedId = allUser.find(user => user.id === id)
+
+                if (existedId) {
+                    console.log("i am in third stage")
+                    res.status(403).json({ message: "Try with another Id" })
+
+                }
+                else if (typeof Number(id) === "number" && Number(id) > 0) {
+                    allUser.push({ id, name, gender, contact, address, photoUrl });
+
+                    fs.writeFile("users.json", JSON.stringify(allUser), (err) => {
+                        if (err) {
+                            res.send('Failed to append file')
+                        } else {
+                            console.log("i am in last stage")
+                            res.send("user has succesfully added")
+                        }
+                    });
+                }
+            }
+            else {
+                res.status(403).json({ error: "the id you provided isn't correct or add all the properties" })
+            }
+        }
+    })
+
+
+}
+
+// 4. updating a random user 
+const updateUser = (req, res) => {
+
+    let newId;
+    const { id, gender, name, contact, address, photoUrl } = req.body
+
+    fs.readFile("users.json", (err, data) => {
+        if (err) {
+            res.status(403).json({ message: "data was not found" })
+        }
+        else {
+            let parsedData = JSON.parse(data);
+            if (!gender || !name || !contact || !address || !photoUrl) {
+                return res.status(403).json({ error: "Please provide gender, name, contact, address, photoUrl property." })
+            }
+            if (id) {
+                newId = id
+            } else {
+                newId = Math.floor(Math.random() * (parsedData.length - 1)) + 1;
+            }
+            const updatedUser = { id: newId, gender, name, contact, address, photoUrl }
+            const userExist = parsedData.find(user => user.id == Number(newId))
+            if (!userExist) {
+                res.status(403).json({ error: "User data not found" })
+            } else if (updatedUser) {
+                parsedData = parsedData.map(user => user.id != Number(newId) ? user : updatedUser)
+                fs.writeFile("users.json", JSON.stringify(parsedData), (err) => {
+                    if (err) {
+                        res.status(500).json({ error: "Internal Server Error" })
+
+                    }
+                    else {
+                        res.status(201).json({ message: "User data updated successfully" })
+                    }
+                })
+
+            }
+
+        }
+    })
+}
+
+
+// 5. updating multiple users 
+const bulkUpdate = (req, res) => {
+    const { id, gender, name, contact, address, photoUrl } = req.body;
+    const updatedUser = { id, gender, name, contact, address, photoUrl };
+    fs.readFile("users.json", (err, data) => {
+        let parsedData = JSON.parse(data)
+        if (err) {
+            res.status(404).json({ error: "User was not found" })
+        }
+        else {
+            if (!id || !gender || !name || !contact || !address || !photoUrl) {
+                res.status(403).json({ message: "Please provide id, name ,gender, contact, address and photoUrl" })
+            };
+
+            id.forEach(singleId => {
+                if (isNaN(singleId) || !singleId) {
+                    res.status(403).json({ error: "id is not valid" })
+                }
+            });
+
+            parsedData.forEach(singleUser => {
+                id.filter(singleId => singleUser.id == singleId ? updateUser(singleUser) : null)
+
+            })
+
+            function updateUser(singleUser) {
+                parsedData = parsedData.map(data => {
+                    data.id == singleUser.id ? { ...updatedUser, id: singleUser.id } : data
+                })
+            };
+            fs.writeFile("users.json", jSON.stringify(parsedData), (err) => {
+                if (err) {
+                    res.status(403).json({ error: "data can not be updated" })
+                }
+                else {
+                    res.status(200).json({ message: "Data updated successfully" })
+                }
+            })
+
+        };
+    })
+}
+
+
+const deleteUser = (req, res) => {
+    const { id } = req.body;
+    fs.readFile("users.json", (err, data) => {
+        let parsedData = JSON.parse(data);
+        if (err) {
+            res.status(403).json({ error: "data was not found" })
+        }
+        else {
+            const remainingUsers = parsedData.filter(data => {
+                return data.id != Number(id)
+            })
+            console.log(remainingUsers);
+            if (isNaN(Number(id)) || !id) {
+                res.status(403).json({ error: "id was not found" })
+            }
+            else if (parsedData.length === remainingUsers.length) {
+                res.status(403).json({ error: "user not found" })
+            }
+            else if (remainingUsers) {
+                fs.writeFile("users.json", JSON.stringify(remainingUsers), (err) => {
+                    if (err) {
+                        res.status(403).json({ message: "data can't delete properly" })
+                    }
+                    else {
+                        res.status(200).json({ message: "data deleted successfully" })
+                    }
+                })
+            }
+        }
+    })
+
+}
+
+
+app.get('/user/random', randomUser)
+app.get('/user/all', allUser)
+app.post('/user/save', createUser)
+app.patch('/user/update', updateUser)
+app.patch('/user/bulk-upadate', bulkUpdate)
+app.delete('/user/delete', deleteUser)
 
 app.listen(port, () => {
     console.log(`running in port ${port}`);
 });
+
+// mport { readFileSync, writeFileSync } from 'fs';
+// import express from "express"
+// import path from 'path';
+// "type": "module",
+//   "engines": {
+//     "node": "14.x"
+//   }
